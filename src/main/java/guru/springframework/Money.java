e20f88e0cd70b1564cc7a5c4e9a7ccb0b480c471
Karin <k.i.krasteva@gmail.com>
2021-02-17 02:13:31 +0200

    TDD - more tests and changes

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,31 +1,53 @@
 package guru.springframework;
 
-public abstract class Money {
+public class Money implements Expression {
 
-    protected int amount;
+    protected final int amount;
+    protected final String currency;
 
-    public abstract Money times(int multiplier);
-
-    public static Money dollar(int amount) {
-        return new Dollar(amount);
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
     }
 
-    public static Money franc(int amount) {
-        return new Franc(amount);
+    protected String currency() {
+        return currency;
+    }
+
+
+    public static Money dollar(int amount){
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount){
+        return new Money(amount, "CHF");
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && this.currency == money.currency;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Money money = (Money) o;
-
-        return amount == money.amount;
+    public Money reduce(Bank bank, String to) {
+        return new Money(amount / bank.rate(this.currency, to), to);
     }
 
     @Override
-    public int hashCode() {
-        return amount;
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public Expression times(int multiplier) {
+        return new Money(amount * multiplier, this.currency);
+    }
+
+    @Override
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
     }
 }
